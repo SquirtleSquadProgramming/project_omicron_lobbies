b88e25956ad6cbdba0a67bac8506cd7fae592a08
Elle Fleming <ellefleming455@gmail.com>
2024-09-14 19:44:26 +1200

    Removed extra debug stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use database::DatabaseError;
-use protocol::{IpAddress, ParseError};
+use protocol::ParseError;
 
 mod database;
 mod protocol;
@@ -24,12 +24,4 @@ impl<T> ConvertError<T> for Result<T, ParseError> {
 
 fn main() {
     database::init();
-    println!("Hello, world!");
-    let test4 = IpAddress::IpV4([192, 168, 1, 100]);
-    let test6 = IpAddress::IpV6([
-        0x2405, 0xda40, 0x1179, 0x8400, 0x20ec, 0xc082, 0x2cf6, 0xc8f4,
-    ]);
-
-    println!("IpV4: {test4}");
-    println!("IpV6: {test6}");
 }
