21695c183eba09a5ef3acb717efa06517fbcd5ce
Elle Fleming <ellefleming455@gmail.com>
2024-09-15 01:24:30 +1200

    Changed key to 'ip:port' from 'ip'

--- src/database/in_memory.rs
+++ src/database/in_memory.rs
@@ -5,6 +5,10 @@ use std::collections::HashMap;
 
 static mut DATABASE: Option<HashMap<String, Lobby>> = None;
 
+fn make_key(ip: IpAddress, port: u16) -> String {
+    format!("{ip}:{port}")
+}
+
 pub fn dbg_database() {
     dbg!(unsafe { &DATABASE });
 }
@@ -21,7 +25,7 @@ pub fn create(lobby: Option<Lobby>) -> Result<(), DatabaseError> {
 
     if let Some(db) = unsafe { &mut DATABASE } {
         let lobby = lobby.unwrap();
-        let key = lobby.host_ip.to_string();
+        let key = make_key(lobby.host_ip, lobby.host_port);
 
         if db.contains_key(&key) {
             return Err(DatabaseError::LobbyAlreadyExists);
@@ -41,7 +45,7 @@ pub fn modify(lobby: Option<Lobby>) -> Result<(), DatabaseError> {
 
     if let Some(db) = unsafe { &mut DATABASE } {
         let lobby = lobby.unwrap();
-        let key = lobby.host_ip.to_string();
+        let key = make_key(lobby.host_ip, lobby.host_port);
 
         if !db.contains_key(&key) {
             return Err(DatabaseError::LobbyDoesNotExist);
@@ -60,19 +64,15 @@ pub fn delete(
     password: Option<String>,
 ) -> Result<(), DatabaseError> {
     if let Some(db) = unsafe { &mut DATABASE } {
-        let key = host_ip.to_string();
+        let key = make_key(host_ip, port);
         let lobby = db.get(&key).ok_or(DatabaseError::LobbyDoesNotExist)?;
 
-        let is_valid = if let Some(password) = password {
+        if let Some(password) = password {
             let password_check = verify(password, &lobby.password)
                 .map_err(|_| DatabaseError::FailedToVerifyPassword)?;
-            lobby.host_port == port && password_check
-        } else {
-            lobby.host_port == port
-        };
-
-        if !is_valid {
-            return Err(DatabaseError::InvalidCredentials);
+            if !password_check {
+                return Err(DatabaseError::InvalidCredentials);
+            }
         }
 
         if db.remove(&key).is_some() {
